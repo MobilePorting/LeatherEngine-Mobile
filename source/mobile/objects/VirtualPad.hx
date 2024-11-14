40d6f9a4e88b1418afc64bd15588e49c88918a26
Lily <egzozu.be.bas@gmail.com>
2024-11-14 18:35:51 +0300

    license our classes

--- source/mobile/objects/VirtualPad.hx
+++ source/mobile/objects/VirtualPad.hx
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2024 Mobile Porting Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package mobile.objects;
 
 import flixel.FlxG;
@@ -37,7 +54,7 @@ enum FlxActionMode {
  * A gamepad.
  * It's easy to customize the layout.
  *
- * @author Mihai Alexandru (M.A. Jigsaw)
+ * @author Lily Ross (mcagabe19)
  */
 class VirtualPad extends FlxSpriteGroup {
 	@:dox(hide) public var buttonLeft2:MobileButton = new MobileButton(0, 0);
